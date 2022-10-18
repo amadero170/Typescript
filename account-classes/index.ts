51bbf2a1d7d8186cb6d153f1b9a04f51a1b1cd3b
amadero170 <84208214+amadero170@users.noreply.github.com>
2022-10-18 11:34:05 -0500

    account classes ts file with inheritance

---
+++ account-classes/index.ts
@@ -0,0 +1,63 @@
+type Individual = {
+  name: string;
+  taxId: string;
+};
+type Joint = Individual[];
+
+type Custodial = [Individual, { name: string }];
+
+type Corporate = Individual;
+
+class Account {
+  account: string = "";
+  ownerType: string;
+  owner: Individual | Joint | Custodial | Corporate;
+
+  constructor(account: string, data) {
+    this.account = account;
+    this.owner = data;
+  }
+}
+class individualAccount extends Account {
+  owner: Individual;
+  constructor(data: Individual, account: string) {
+    super(account, data);
+
+    this.ownerType = "individual owner";
+  }
+}
+class jointAccount extends Account {
+  owner: Joint;
+  constructor(data: Joint, account: string) {
+    super(account, data);
+    this.owner = data;
+    this.ownerType = "joint ownership";
+  }
+}
+class custodialAccount extends Account {
+  owner: Custodial;
+  constructor(data: Custodial, account: string) {
+    super(account, data);
+    this.owner = data;
+    this.ownerType = "custodial ownership";
+  }
+}
+class corporateAccount extends Account {
+  owner: Corporate;
+  constructor(data: Corporate, account: string) {
+    super(account, data);
+    this.owner = data;
+    this.ownerType = "corporate ownership";
+  }
+}
+
+const lolo = new individualAccount({ name: "oli", taxId: "h42n3" }, "acc46282");
+console.log(lolo);
+const lolo2 = new jointAccount(
+  [
+    { name: "oli", taxId: "h42n3" },
+    { name: "mariana", taxId: "uuuuu" },
+  ],
+  "acc46282"
+);
+console.log(lolo2);
